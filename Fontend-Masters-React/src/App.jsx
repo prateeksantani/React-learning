c5b4a28e5f6d8e708c94f159a660e37de19fff11
Prateek Santani <prateeksantani98@gmail.com>
2024-06-12 10:23:08 +0530

    pure react createElement no JSX

---
+++ Fontend-Masters-React/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Luna"),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet, { animal: "Dog", breed: "Shitzu" }),
+    React.createElement(Pet),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+// root.render(<App />);
